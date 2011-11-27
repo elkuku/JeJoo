42f2c622156a9c200c5e2479e90200304c4f1099
Nikolai Plath <der.el.kuku@gmail.com>
2011-11-27 04:04:28 -0500

    Add descriptions to configuration.dist.php

--- scripts/configuration.dist.php
+++ scripts/configuration.dist.php
@@ -1,18 +1,20 @@
 <?php
 class JConfig
 {
-	
-	public $outputPath = '';
-	
-	public $platformPath = '';
-	
-	
-	
+    /** A directory outside the web root - accessible by the script */
+	public $buildPath  = 'CHANGE ME';
+
+	/* A web acessible target path - not used yet */
+	public $targetPath = '';
+
+
+/* Not used yet */
+
 	public $dbtype		= 'mysql';
 	public $host		= '127.0.0.1';
-	public $user		= 'root';
+	public $user		= '';
 	public $password	= '';
-	public $db			= '';
+	public $db		= '';
 	public $dbprefix	= '';
 	public $ftp_host	= '127.0.0.1';
 	public $ftp_port	= '21';
@@ -20,14 +22,14 @@ class JConfig
 	public $ftp_pass	= '';
 	public $ftp_root	= '';
 	public $ftp_enable	= 0;
-	public $tmp_path	= 'tmp';
-	public $log_path	= 'logs';
+	public $tmp_path	= '';
+	public $log_path	= '';
 	public $mailer		= 'mail';
 	public $mailfrom	= 'admin@localhost.home';
 	public $fromname	= '';
 	public $sendmail	= '/usr/sbin/sendmail';
 	public $smtpauth	= '0';
-	public $smtpsecure = 'none';
+	public $smtpsecure	= 'none';
 	public $smtpport	= '25';
 	public $smtpuser	= '';
 	public $smtppass	= '';
